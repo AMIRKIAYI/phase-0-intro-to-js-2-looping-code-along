97c42b6a1679ff42675fce949a87290b7724a5ee
AMIRKIAYI <amirkiayi516@gmail.com>
2024-05-28 12:47:51 +0300

    index.js file update

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+
+const names = ["Charlie", "Samip", "Ali"];
+const event = "birthday";
+ 
+
+function writeCards(names, event) {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+
+
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  
+  countDown(10); 
+
+  
+
+  
